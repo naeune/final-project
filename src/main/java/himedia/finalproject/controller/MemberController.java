025210a582bd506ec10317f4ecf3c993ac508bb9
naeune <ivaeune@gmail.com>
2022-12-27 23:01:22 +0900

    ~221227

---
+++ src/main/java/himedia/finalproject/controller/MemberController.java
@@ -0,0 +1,9 @@
+package himedia.finalproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+	
+	
+}
---
+++ src/test/java/himedia/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package himedia.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
